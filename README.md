941337ef04b31b96fdaa1bba4d532ed6faa13990
15077693d <oscaryiu.lapsang@gmail.com>
2022-02-12 16:02:55 +0800

    lesson 3

---
+++ README.md
@@ -0,0 +1,10 @@
+my-threejs-journey
+I'm learning three.js! https://threejs-journey.com/
+
+12/2/2022: lesson3 - Basic Scene (Box and Camera Panel)</br>
+learning basic elements on create scene (Scene, Objects, Camera, Renderer)
+
+- Scene: A container of Objects etc. THREE.Mesh
+- Object: Object property can be defined by THREE.MeshBasicMaterial
+- Camera: Point of view on scene
+- Renderer: config canvas and use for render scene with camera
---
+++ lesson3/script.js
@@ -0,0 +1,77 @@
+console.log("hello three.js!");
+let defaultProps = {
+  boxColor: 0xff0000,
+  boxX: 1,
+  boxY: 1,
+  boxZ: 1,
+  cameraPositionX: 0,
+  cameraPositionY: 0,
+  cameraPositionZ: 3,
+  cameraWidth: 800,
+  cameraHeight: 600,
+};
+function renderBox(props = defaultProps) {
+  const {
+    boxColor,
+    boxX,
+    boxY,
+    boxZ,
+    cameraPositionX,
+    cameraPositionY,
+    cameraPositionZ,
+    cameraWidth,
+    cameraHeight,
+  } = props;
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Objects(with material)
+  const geometry = new THREE.BoxGeometry(boxX, boxY, boxZ);
+  const material = new THREE.MeshBasicMaterial({
+    color: Number(boxColor),
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  // Camera
+  // The position property is an object with three relevant properties: x, y and z. By default, Three.js considers the forward/backward axis to be z.
+  // Sizes
+  const sizes = {
+    width: Number(cameraWidth),
+    height: Number(cameraHeight),
+  };
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+  camera.position.x = cameraPositionX;
+  camera.position.y = cameraPositionY;
+  camera.position.z = cameraPositionZ;
+  scene.add(camera);
+
+  // Canvas
+  const canvas = document.querySelector("canvas.webgl");
+
+  // Renderrer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera);
+}
+renderBox();
+const ids = [
+  "boxColor",
+  "boxX",
+  "boxY",
+  "boxZ",
+  "cameraPositionX",
+  "cameraPositionY",
+  "cameraPositionZ",
+  "cameraWidth",
+  "cameraHeight",
+];
+for (let index = 0; index < ids.length; index++) {
+  const id = ids[index];
+  document.getElementById(id).addEventListener("change", (e) => {
+    defaultProps = { ...defaultProps, [id]: e.target.value };
+    renderBox(defaultProps);
+  });
+}
